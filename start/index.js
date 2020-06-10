05b619b45a1a2c5e3d47b51a5030e47bff2cb238
alisha musamabik <alisha.musamabik@gmail.com>
2020-06-10 14:56:27 +0100

    project mastering Javascript - 30 day challenege

---
+++ start/index.js
@@ -0,0 +1,7 @@
+
+
+// ******************inital ***************
+// window.addEventListener('keydown',function(e){
+  //   const audio = document.querySelector(`audio[data-key=${e.keyCode}]`);
+  //   console.log(audio);
+  // });
